ef7af422c3795664e27cf6f31c55dc930f39c7fb
acse-lz1219 <lz1219@ic.ac.uk>
2019-10-22 15:42:26 +0100

    midification

--- group_work.py
+++ group_work.py
@@ -18,17 +18,25 @@ def get_easting_northing_flood_probability_band(easting,northing):
     bands = np.array(location_data['Probability Band'])
     return bands
 
-def get_flood_cost(postcodes,probability_bands):
+def get_flood_cost(postcodes):
     a = postcodes
-    b = probability_bands
-    total_value_data = test_data.loc[(test_data.Postcode == a)& (test_data['Probability Band'] == b)]
+    total_value_data = test_data.loc[(test_data['Postcode'].isin (a))]
+    total_value_data = total_value_data.set_index('Postcode')
+    total_value_data = total_value_data.reindex(index = a)
+    total_value_data = total_value_data.reset_index()
+    total_value_data = total_value_data.fillna(0)
     cost_value = np.array(total_value_data['Total Value'])*0.05
     return cost_value
 
 def get_sorted_flood_probability(postcodes):
-    replace_data = test_data['Probability Band'].replace(['High','Medium','Low','Very Low','Zero'],[4,3,2,1,0])
-    test_data['replace_data'] = replace_data
-    updated = test_data.sort_values(by = ['replace_data','Postcode'],ascending = (False,True))
+    a = postcodes
+    fp_data = test_data.loc[(test_data['Postcode'].isin (a))]
+    fp_data = fp_data.set_index('Postcode')
+    fp_data = fp_data.reindex(index = a)
+    fp_data = fp_data.reset_index()
+    replace_data = fp_data['Probability Band'].replace(['High','Medium','Low','Very Low','Zero'],[4,3,2,1,0])
+    fp_data['replace_data'] = replace_data
+    updated = fp_data.sort_values(by = ['replace_data','Postcode'],ascending = (False,True))
     updated = updated.set_index('Postcode')    
     return updated(['Probability Band'])
     
