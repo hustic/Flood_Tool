91ed7dcdf0f405c7afe6d005b600b30bd9b4bf15
acse-nt719 <55123440+ntrin@users.noreply.github.com>
2019-10-25 11:27:56 +0100

    Update API_4_Test.py

--- flood_tool/API_4_Test.py
+++ flood_tool/API_4_Test.py
@@ -1,24 +1,38 @@
+'''
+Function extracts rain data for a single day. Will return the average rainfall in quadrants whereby
+the center is defined by the user. Will also return the average rainfall against time for each quadrant.
+
+Inputs:
+    date: date of interest
+        string
+    easting_lim, northing_lim: easting and northing of the specified center
+        int or float
+    latlong: 
+        Boolean
+        If True, the easting_lim and northing_lim are input as lattitude and longitude.
+
+'''
 import requests
 import json
 # import tool
-# import geo
+import geo
 from math import sqrt
 import numpy as np
 import pandas as pd
-import csv
-from flask import Flask
 import matplotlib.pyplot as plt
 
-def historic_API(date):
+def historic_API(date, easting_lim, northing_lim, latlong=False):
     url = 'https://environment.data.gov.uk/flood-monitoring/archive/readings-full-' + date + '.csv'
-    #print(url)
     data_csv = pd.read_csv(url)
     df=pd.DataFrame(data=data_csv)
-    #print(df)
+
     df = df.groupby('parameter')
     df = df.get_group('rainfall')
     df = df.reset_index()
-    #print(df)
+
+    if latlong == True:
+        easting_lim, northing_lim = geo.get_easting_northing_from_lat_long(easting_lim, northing_lim)
+    
     values = []
     dates = []
     stations = []
@@ -26,7 +40,7 @@ def historic_API(date):
     easting = []
     station_list = []
     northeast = []
-    for row in range(0, 10):
+    for row in range(0, len(df)):
         historic_values = df.loc[row, 'value']
         historic_date = df.loc[row, 'dateTime']
         station = df.loc[row, 'stationReference']   
@@ -52,60 +66,50 @@ def historic_API(date):
             northing.append(north)
             easting.append(east)
 
-        #print(historic_date)
         values.append(historic_values)
         dates.append(historic_date)
 
-        # if north[row] > 286822 and east[row] > 406689:
-        #     northeast.append(historic_values)
-
-        #print(values)
     historic_rain = pd.DataFrame({'dates':dates[:], 'station':stations[:], 'northing':northing[:], 'easting':easting[:], 'values':values[:]})
     historic_rain['dates'] = historic_rain['dates'].map(lambda x: x.rstrip('Z'))
-    historic_rain['date'], historic_rain['time'] = historic_rain['dates'].str.split('T', 1).str
+    # for line in historic_rain['dates']:
+    #     if ':01T' in historic_rain['dates']:
+    #         historic_rain['dates'][row] = historic_rain['dates'][row].str.replace(':01T', ':00T')
 
-    #rain = historic_rain.sort_index()
-    #historic_rain.set_index('time', drop=True, append=False, inplace=True, verify_integrity=False)
-    historic_rain = historic_rain.drop('dates', axis=1).drop('date', axis=1).sort_values(by='time', ascending=True)
-    #print(historic_rain)
-    #print(historic_rain.dtypes)
-    group = historic_rain.groupby(['station', 'northing', 'easting', 'time'], as_index=False)['values'].mean()
-    #i = historic_rain.groupby(['station', 'time']).cumcount()
-    historic_rain.set_index(['station']).time
-    print('g', group)
-    # northeast = group[]
-    historic_rain = pd.DataFrame({'dates':dates[:], 'station':stations[:], 'northing':northing[:], 'easting':easting[:], 'values':values[:]})
-    historic_rain['dates'] = historic_rain['dates'].map(lambda x: x.rstrip('Z'))
     historic_rain['date'], historic_rain['time'] = historic_rain['dates'].str.split('T', 1).str
+    
+    # pattern = '*:01'
+    # for line in historic_rain['time']:
+    #     if fnmatch(line, pattern):
+    #         historic_rain.dates.loc[row] = historic_rain.dates.loc[row].replace('*$.:01', ':00', regex=True)
+    #         # historic_rain['dates'][index=row] = historic_rain['dates'][index=row].replace(r'*$.:01', ':00')
+
     historic_rain = historic_rain.drop('dates', axis=1).drop('date', axis=1).sort_values(by='time', ascending=True)
 
-    northeast = historic_rain.loc[(historic_rain.northing > 286822) & (historic_rain.easting > 406689)]
+    northeast = historic_rain.loc[(historic_rain.northing > northing_lim) & (historic_rain.easting > easting_lim)]
     print('ne', northeast)
     northeast_averageT = northeast.groupby('time')['values'].mean().reset_index()
     print('neAT', northeast_averageT)
     northeast_average = northeast['values'].mean()
 
-    southeast = historic_rain.loc[(historic_rain.northing < 286822) & (historic_rain.easting > 406689)]
+    southeast = historic_rain.loc[(historic_rain.northing < northing_lim) & (historic_rain.easting > easting_lim)]
     print('se', southeast)
     southeast_average = southeast['values'].mean()
     southeast_averageT = southeast.groupby('time')['values'].mean().reset_index()
     print('seA', southeast_average)
 
-    northwest = historic_rain.loc[(historic_rain.northing > 286822) & (historic_rain.easting < 406689)]
+    northwest = historic_rain.loc[(historic_rain.northing > northing_lim) & (historic_rain.easting < easting_lim)]
     print('nw', northwest)
     northwest_average = northwest['values'].mean()
     northwest_averageT = northwest.groupby('time')['values'].mean().reset_index()
     print('nwA', northwest_average)
 
-    southwest = historic_rain.loc[(historic_rain.northing < 286822) & (historic_rain.easting < 406689)]
+    southwest = historic_rain.loc[(historic_rain.northing < northing_lim) & (historic_rain.easting < easting_lim)]
     print('sw', southwest)
     southwest_average = southwest['values'].mean()
     southwest_averageT = southwest.groupby('time')['values'].mean().reset_index()
     print('swA', southwest_average)
 
-    # plt.plot(northeast['time'], northeast_average['values'])
-    # print(historic_rain)
-    # print(average)
+
     plt.figure(1)
     scale_ls = range(4)
     index_ls = ['NE', 'SE', 'NW', 'SW']
@@ -113,57 +117,43 @@ def historic_API(date):
     plt.xticks(scale_ls, index_ls)
     plt.xlabel('Quadrant')
     plt.ylabel('Average Rainfall (mm)')
-    plt.ylim((0, 0.1))
     plt.bar(scale_ls,area_data )
     plt.show()
 
-    plt.figure(2)
+    fig, ax = plt.subplots()
     plt.title('England Rainfall Against Time for ' + date)
+
     plt.subplot(2, 2, 1)
     plt.plot(northwest_averageT['time'], northwest_averageT['values'])
     plt.xlabel('Time')
-    plt.xticks(rotation=30)
+    plt.xticks(np.arange(0, 96, 4), rotation=30)
     plt.ylabel('Average Rainfall (mm)')
     plt.title('North West')
 
     plt.subplot(2, 2, 2)
     plt.plot(northeast_averageT['time'], northeast_averageT['values'])
     plt.xlabel('Time')
-    plt.xticks(rotation=30)
+    plt.xticks(np.arange(0, 96, 4), rotation=30)
     plt.ylabel('Average Rainfall (mm)')
     plt.title('North East')
 
     plt.subplot(223)
     plt.plot(southwest_averageT['time'], southwest_averageT['values'])
     plt.xlabel('Time')
-    plt.xticks(rotation=30)
+    plt.xticks(np.arange(0, 96, 4), rotation=30)
     plt.ylabel('Average Rainfall (mm)')
     plt.title('South West')
     
     plt.subplot(224)
     plt.plot(southeast_averageT['time'], southeast_averageT['values'])
     plt.xlabel('Time')
-    plt.xticks(rotation=30)
+    plt.xticks(np.arange(0, 96, 4), rotation=30)
     plt.ylabel('Average Rainfall (mm)')
     plt.title('South East')
     
-    plt.subplots_adjust(left=None, bottom=-0.4, top=0.95, right=None)
+    plt.subplots_adjust(left=0.1, bottom=0.05, top=0.9, right=0.95, hspace=0.5)
 
     plt.show()
 
 
-
-
-
-
-
-
-historic_API('2019-01-01')
-app = Flask(__name__)
-
-# @app.route('/')
-# def hello():
-#     return "hello"
-
-# if __name__ == '__main__':
-#     app.run()
+historic_API('2019-01-03', 406689, 286822)
